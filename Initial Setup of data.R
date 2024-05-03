7fe6b499e9d286a02997749b11d99dbf541ea950
donna white <dwhite5@seattleu.edu>
2024-05-02 18:07:05 -0700

    Initial setup of data and libraries for week 5 multiple regression

---
+++ Initial Setup of data.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(stats)
+library(ggplot2)
+library(lmtest)
+library(car)
+
+# Load data into project
+
+load(url("https://www.dropbox.com/s/cnwtcr096szm8im/omsba_5112_birthweight.rdata?raw=1"))
+
+my_data <- birthweight
+
+head(my_data)
